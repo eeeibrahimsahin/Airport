007d3d220ed2508cad3d86c077e47634653aa670
eeeibrahimsahin <56609130+eeeibrahimsahin@users.noreply.github.com>
2021-09-20 21:51:14 +0200

    day_2_assignment_1

--- src/airport/Application.java
+++ src/airport/Application.java
@@ -4,19 +4,24 @@ import airport.model.Airplane;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Test");
-        Airplane airplane1 = new Airplane("ABC123", 60,
+        Airplane airplane1 = new Airplane("ABC123", 33,
                 0, false, 600);
-        Airplane airplane2 = new Airplane("DDD888", 60,
+        Airplane airplane2 = new Airplane("DDD888", 33,
                 0, false, 600);
 
         System.out.println(airplane1.loadPassengersWithNumberOfPassengers(43));
         System.out.println(airplane1.takeOff());
+        airplane1.setFlying(true);
+        System.out.println(airplane1.takeOff());
         System.out.println(airplane2.loadPassengersWithNumberOfPassengers(23));
+        airplane2.setFlying(true);
+        System.out.println(airplane2.takeOff());
+        airplane1.setFlying(false);
         System.out.println(airplane1.land());
         System.out.println(airplane1.unloadPassengers());
         System.out.println(airplane2.takeOff());
-        System.out.println(airplane1.land());
+        airplane2.setFlying(false);
+        System.out.println(airplane2.land());
         System.out.println(airplane2.unloadPassengers());
     }
 }
--- src/airport/model/Airplane.java
+++ src/airport/model/Airplane.java
@@ -11,26 +11,79 @@ public class Airplane {
     }
 
     public String loadPassengersWithNumberOfPassengers(int numberOfPassengers) {
+        if (numberOfPassengers > this.maximumNumberOfPassengers) {
+            this.currentNumberOfPassengers = this.maximumNumberOfPassengers;
+            return "Airplane " + this.id + " charges " + this.maximumNumberOfPassengers + " passengers, "
+                    + (numberOfPassengers - this.maximumNumberOfPassengers) + " do not fit.";
+        }
         this.currentNumberOfPassengers = numberOfPassengers;
-        return "Airplane " + this.id + " loads " + this.currentNumberOfPassengers + " passengers.";
+        return "Airplane " + this.id + " charges " + this.currentNumberOfPassengers + " passengers.";
+
     }
 
     public String unloadPassengers() {
-        String returnText = "Airplane " + this.id + " unloads " + this.currentNumberOfPassengers + " passengers.";
+        String returnText = "Airplane " + this.id + " discharges " + this.currentNumberOfPassengers + " passengers.";
         this.currentNumberOfPassengers = 0;
         return returnText;
     }
 
     public String takeOff() {
+        if (isFlying) {
+            return "Airplane " + this.id + " can not take off, because we are already flying.";
+        } else if (!isFlying) {
+            return "Airplane " + this.id + " can not land, because we are still on the ground.";
+        }
         this.isFlying = true;
-        return "Airplane " + this.id + " takes off.";
+        return "Airplane " + this.id + " rises.";
     }
 
     public String land() {
+        if (isFlying) {
+            return "Airplane " + this.id + " can not take off, because we are already flying.";
+        }
         this.isFlying = false;
         return "Airplane " + this.id + " lands.";
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getMaximumNumberOfPassengers() {
+        return maximumNumberOfPassengers;
+    }
+
+    public void setMaximumNumberOfPassengers(int maximumNumberOfPassengers) {
+        this.maximumNumberOfPassengers = maximumNumberOfPassengers;
+    }
+
+    public int getCurrentNumberOfPassengers() {
+        return currentNumberOfPassengers;
+    }
+
+    public void setCurrentNumberOfPassengers(int currentNumberOfPassengers) {
+        this.currentNumberOfPassengers = currentNumberOfPassengers;
+    }
+
+    public boolean isFlying() {
+        return isFlying;
+    }
+
+    public void setFlying(boolean flying) {
+        isFlying = flying;
+    }
+
+    public double getCruiseSpeed() {
+        return cruiseSpeed;
+    }
+
+    public void setCruiseSpeed(double cruiseSpeed) {
+        this.cruiseSpeed = cruiseSpeed;
+    }
 
     private String id;
     private int maximumNumberOfPassengers;
