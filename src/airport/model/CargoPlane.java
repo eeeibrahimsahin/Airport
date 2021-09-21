c0bb08a365853bc534b3b99602d3aa4641eaca3a
eeeibrahimsahin <56609130+eeeibrahimsahin@users.noreply.github.com>
2021-09-21 10:41:34 +0200

    updated.

--- src/airport/model/CargoPlane.java
+++ src/airport/model/CargoPlane.java
@@ -7,7 +7,6 @@ public class CargoPlane extends Airplane {
 
     @Override
     protected String loadPlane(int amount) {
-        setupPlane(this);
         if (amount > this.getMaximumNumberOfLoad()) {
             this.setCurrentNumberOfLoad(this.getMaximumNumberOfLoad());
             return "Cargo Plane " + this.getId() + " loads " + amount+ " tons of cargo,"
